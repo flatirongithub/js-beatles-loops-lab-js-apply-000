6defe351e7f610151b09f6a62950d78ad72bc478
N <nirav2000+flatiron@gmail.com>
2018-05-15 20:41:59 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,24 @@
+function theBeatlesPlay (musicians, instruments) {
+  var array = [];
+  for (var m in musicians) {
+    array.push(musicians[m] + ' plays ' + instruments[m]);
+  }
+  return array;
+}
+
+function johnLennonFacts (facts) {
+  var fact;
+  while (fact in facts) {
+    facts[fact].push(" a");
+  }
+  return facts;
+}
+
+function iLoveTheBeatles (n) {
+  var array = [];
+  do {
+    array.push( "I love the Beatles!");
+    n++;
+  } while (n<15);
+  return array;
+}
